6fea59f25b7e67dd3eee50b78094717db9d43879
iskyldrm <isakyildirim@outlook.com>
2022-06-01 12:50:16 +0300

    Add project files.

---
+++ Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace APIConsumer
+{
+    public class Customer
+    {
+        public string CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text.Json;
+
+namespace APIConsumer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create("https://localhost:44304/weatherforecast/1");
+
+            httpWebRequest.Method = "GET";
+            String test = String.Empty;
+
+            using (HttpWebResponse response = (HttpWebResponse)httpWebRequest.GetResponse())
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                test = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+            }
+            var CustomerList = JsonSerializer.Deserialize<List<Customer>>(test, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            foreach (Customer c in CustomerList)
+            {
+                Console.WriteLine(c.CustomerId);
+            }
+        }
+    }
+}
